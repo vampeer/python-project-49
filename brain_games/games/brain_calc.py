476596ad3803fd77c62f964e365d9583a860662c
Andriy Vasylenko <andrewvasilen@gmail.com>
2025-01-25 15:06:23 +0200

    fix brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,5 +1,6 @@
 import operator
 import random
+
 from brain_games.games.brain_even import generate_number
 
 RULES = "What is the result of the expression?"
@@ -20,7 +21,7 @@ def make_question():
     first_num = generate_number()
     second_num = generate_number()
     operator = generate_operation()
-    question = f"Question: {first_num}{operator}{second_num}"
+    question = f"Question: {first_num} {operator} {second_num}"
     answer = correct_answer(first_num, second_num, operator)
     return (question, answer)
 
